98945a236cc19cb9a289891f19fe1b0612fdcb1e
nika kobaidze <nika.kobaidze@gmail.com>
2025-02-25 23:58:09 +0400

    [ADD] buttons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import webbrowser
+from telebot import types
 
 bot = telebot.TeleBot('1974247687:AAGe4o8naTcpLA2sTg5eL-GJVAyy54gTwlA')
 
@@ -15,5 +16,14 @@ def info(message):
 @bot.message_handler(commands=['browser'])
 def browser(message):
     webbrowser.open('https://google.com')
+    
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    temp = types.InlineKeyboardMarkup()
+    temp.add(types.InlineKeyboardButton("open page", url="https://google.com"))
+    temp.add(types.InlineKeyboardButton("Delete", callback_data='delete'))
+    temp.add(types.InlineKeyboardButton("Update", callback_data='edit'))
+    
+    bot.reply_to(message, 'Good!', reply_markup=temp)
     
 bot.polling(none_stop=True)
